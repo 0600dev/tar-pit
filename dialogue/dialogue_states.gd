edce91dcfafea8c9c4e62da8b2182d677c327f29
0600dev <0600dev@gmail.com>
2024-12-01 10:30:15 -0600

    changed ending

--- dialogue/dialogue_states.gd
+++ dialogue/dialogue_states.gd
@@ -12,7 +12,7 @@ func _process(_delta):
 func _on_death():
 	# wait after dialogue then quit game
 	await get_tree().create_timer(1).timeout
-	get_tree().quit()
+	get_tree().change_scene_to_file("res://scenes/managers_tools_scenes/you_found_it.tscn")
 
 func _timeout():
 	# dialogue pause
