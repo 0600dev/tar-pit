1d9c5cf2312bf740553605c4a19a0308b11e4cca
0600dev <0600dev@gmail.com>
2024-12-05 08:03:11 -0600

    dialogue manager deletion

--- scripts/managers_tools_scripts/game_manager.gd
+++ scripts/managers_tools_scripts/game_manager.gd
@@ -1,7 +1,6 @@
 extends Node2D
 class_name GameManager
 #
-var dialogue_states : DialogueStates
 # portrait
 enum Portraits {TAR_SPIRIT, KASH, CAPTAIN}
 @onready var portrait : Portraits = Portraits.TAR_SPIRIT
@@ -14,9 +13,6 @@ enum Portraits {TAR_SPIRIT, KASH, CAPTAIN}
 @onready var sword_shotel = $CanvasGroup/inventory/items/sword_shotel
 @onready var sword_takoba = $CanvasGroup/inventory/items/sword_takoba
 @onready var water_gourd = $CanvasGroup/inventory/items/water_gourd
-# dialogue
-@export var dialogue_resource : DialogueResource
-var dialogue_start : String = "start"
 # events
 @onready var orc_scene_change = $orb_scene_change
 # screenchange
@@ -32,7 +28,6 @@ func _ready() -> void:
 	# portrait animation
 	portrait_pic.play("nothing")
 	# first dialogue
-	#DialogueManager.show_dialogue_balloon(dialogue_resource, "temple_scene_start")
 	#item visibility
 	coin_purse.visible = false
 	paddle_doll.visible = false
@@ -77,8 +72,6 @@ func _portrait_state():
 		sword_takoba.visible = false
 		water_gourd.visible = true
 
-		# play dialogue if this character
-		DialogueManager.show_dialogue_balloon(dialogue_resource, "desert_dialogue")
 		
 	elif portrait == Portraits.CAPTAIN:
 		portrait_pic.play("captain")
@@ -97,33 +90,33 @@ func _on_coin_purse_pressed():
 	# disable button
 	coin_purse.disabled = true
 	# play "wrong item" dialogue
-	DialogueManager.show_dialogue_balloon(dialogue_resource, "wrong_item")
+
 
 func _on_paddle_doll_pressed():
 	paddle_doll.self_modulate.a = 0
 	#
 	paddle_doll.disabled = true
 	# play "right item" dialogue
-	DialogueManager.show_dialogue_balloon(dialogue_resource, "right_item")
+
 
 func _on_sword_shotel_pressed():
 	sword_shotel.self_modulate.a = 0
 	#
 	sword_shotel.disabled = true
 	#
-	DialogueManager.show_dialogue_balloon(dialogue_resource, "wrong_item")
+
 
 func _on_sword_takoba_pressed():
 	sword_takoba.self_modulate.a = 0
 	#
 	sword_takoba.disabled = true
 	#
-	DialogueManager.show_dialogue_balloon(dialogue_resource, "wrong_item")
+
 
 func _on_water_gourd_pressed():
 	water_gourd.self_modulate.a = 0
 	#
-	DialogueManager.show_dialogue_balloon(dialogue_resource, "wrong_item")
+
 
 
 
@@ -137,4 +130,3 @@ func _on_orb_scene_change_gui_input():
 		sword_takoba.visible = true
 		water_gourd.visible = true	
 	# play "item brought" dialogue on play	
-		DialogueManager.show_dialogue_balloon(dialogue_resource, "item_brought")
